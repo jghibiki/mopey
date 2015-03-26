8001aa1cca6560d5e83ab52b1d93a6cc1bf5e567
jghibiki <jghibiki.games@gmail.com>
2015-03-26 11:36:44 -0500

    Added a script for setting up the database and inserting default users

---
+++ api/src/SetUp.py
@@ -0,0 +1,67 @@
+import peewee
+from Models import *
+from uuid import uuid4
+import hashlib
+
+if __name__ == "__main__":
+
+    print "###########################"
+    print "## Starting Server Setup ##"
+    print "###########################"
+
+    db = peewee.PostgresqlDatabase('postgres', host='db', user='postgres')
+
+    print "Wiping database and creating tables..."
+    tables = [
+        User,
+        AccessToken,
+        Admin
+        ]
+    db.drop_tables(tables, safe=True)
+    db.create_tables(tables, safe=True)
+    print "Finished creating tables!"
+
+
+    print "Adding user jghibiki..."
+    jghibiki = User.create(
+           username = "jghibiki",
+           password = "cf719217b63d276f7e74047df2539449",
+           firstName = "Jordan",
+           lastName = "Goetze",
+           email = "jghibiki.games@gmail.com",
+           karma = 0,
+           strikes = 0,
+        )
+    if(jghibiki == None):
+        raise "Failed to add user jghibiki"
+
+    print "Making jghibiki an admin..."
+    jghibiki_admin = Admin.create(jghibiki.key)
+    if(jghibiki_admin == None):
+        raise "Failed to make use jghibiki an admin"
+    print "Success!"
+
+    print "Adding user jpisabrony"
+    jpisabrony = User.create(
+           username = "jpisabrony",
+           password = "",
+           firstName = "Josh",
+           lastName = "Pohl",
+           email = "josh.pohl17@gmail.com",
+           karma = 0,
+           strikes = 0,
+        )
+    if(jpisabrony == None):
+        raise "Failed to add user jpisabrony"
+
+    print "Making jghibiki an admin..."
+    jpisabrony_admin = Admin.create(jp_isabrony.key)
+    if(jpisabrony == None):
+        raise "Failed to make use jpisabrony an admin"
+    print "Success!"
+
+    print "############################"
+    print "## Completed Server Setup ##"
+    print "############################"
+
+
