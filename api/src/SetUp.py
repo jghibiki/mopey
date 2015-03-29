fdc15c4b7200e41201ea4e95237a518ed4a6ca9e
jghibiki <jghibiki.games@gmail.com>
2015-03-29 16:27:43 -0500

    Add message to index page

--- api/src/SetUp.py
+++ api/src/SetUp.py
@@ -22,48 +22,44 @@ def main():
     db.create_tables(tables, safe=True)
     print "Finished creating tables!"
 
-
     print "Adding user jghibiki..."
     jghibiki = User.create(
-           username = "jghibiki",
-           password = "7418acc26027cf4a026c35d21f468329",
-           firstName = "Jordan",
-           lastName = "Goetze",
-           email = "jghibiki.games@gmail.com",
-           karma = 100000,
-           strikes = 0,
-        )
-    if(jghibiki == None):
+       username="jghibiki",
+       password="7418acc26027cf4a026c35d21f468329",
+       firstName="Jordan",
+       lastName="Goetze",
+       email="jghibiki.games@gmail.com",
+       karma=100000,
+       strikes=0,
+    )
+    if(jghibiki is None):
         raise "Failed to add user jghibiki"
 
     print "Making jghibiki an admin..."
     jghibiki_admin = Admin.create(user=jghibiki)
-    if(jghibiki_admin == None):
+    if(jghibiki_admin is None):
         raise "Failed to make use jghibiki an admin"
     print "Success!"
 
     print "Adding user jpisabrony"
     jpisabrony = User.create(
-           username = "jpisabrony",
-           password = "9c4faf82d5d151461fe18b818f4a09f6",
-           firstName = "Josh",
-           lastName = "Pohl",
-           email = "josh.pohl17@gmail.com",
-           karma = 100000,
-           strikes = 0,
-        )
-    if(jpisabrony == None):
+       username="jpisabrony",
+       password="9c4faf82d5d151461fe18b818f4a09f6",
+       firstName="Josh",
+       lastName="Pohl",
+       email="josh.pohl17@gmail.com",
+       karma=100000,
+       strikes=0,
+    )
+    if(jpisabrony is None):
         raise "Failed to add user jpisabrony"
 
     print "Making jghibiki an admin..."
     jpisabrony_admin = Admin.create(user=jpisabrony)
-    if(jpisabrony == None):
+    if(jpisabrony is None):
         raise "Failed to make use jpisabrony an admin"
     print "Success!"
 
     print "############################"
     print "## Completed Server Setup ##"
     print "############################"
-
-
-
