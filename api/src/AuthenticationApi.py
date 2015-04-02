48c9c7010f2a7edb2f2c7d29fc9c22015687be09
jghibiki <jghibiki.games@gmail.com>
2015-04-02 13:49:02 -0500

    Adds authented endpoints

--- api/src/AuthenticationApi.py
+++ api/src/AuthenticationApi.py
@@ -1,47 +1,39 @@
 from Models import *
-from flask import jsonify
+from flask import jsonify, request, make_response
 import hashlib
 import UserApi
 from GeneralApiException import GeneralApiException
 from uuid import uuid4
 from datetime import datetime, timedelta
+from functools import wraps, update_wrapper
 
 def authentication(json):
-    returnedUser = UserApi.getUserDatabase(json["username"])
+    username = json["username"]
+    password = json["password"]
+    if username is None or username is "":
+        return GeneralApiException("Bad request", status_code=400)
+    if password is None or password is "":
+        return GeneralApiException("Bad request", status_code=400)
 
+    returnedUser = UserApi.getUserDatabase(username)
     if(returnedUser == None):
         raise GeneralApiException("Invalid user name and password combination.", status_code=200)
 
-    md5 = hashlib.md5(json["password"]).hexdigest()
+    md5 = hashlib.md5(password).hexdigest()
     if(returnedUser.password != str(md5)):
         raise GeneralApiException("Invalid user name and password combination.", status_code=200)
 
+    # Issue the new token
     returnedAccessToken = AccessToken.create(token=str(uuid4()), expirationDate=datetime.utcnow() + timedelta(hours=1), user=returnedUser)
 
     payload = {"access_token" : returnedAccessToken.token, "expiration_date" : returnedAccessToken.expirationDate}
-
     return jsonify(payload)
-
-
-def validateUserAuthentication(returnedUser, accessTokenPassed):
-    validAccessToken = False
-
-    for access_token in returnedUser.accessTokens:
-        if(accessTokenPassed == access_token.token):
-            if(access_token.expirationDate > datetime.utcnow()):
-               validAccessToken = True
-               break
-
-    if(validAccessToken == False):
-        raise GeneralApiException("Authentication error", status_code=200)
-
-    return
 
 
 def getAccessTokenDatabase(key):
     returnedAuth = None
     if(key == None):
-        return 400
+        return GeneralApiException("Bad request", status_code=400)
     try:
         returnedAuth = AccessToken.get(AccessToken.key == key)
     except:
@@ -50,3 +42,55 @@ def getAccessTokenDatabase(key):
     return returnedAuth
 
 
+def validateAuthToken(token, isAdmin=False):
+    if token is None:
+        return False
+
+    realToken = None
+    try:
+        realToken = AccessToken.get(AccessToken.token == token)
+    except:
+        return False
+
+    if(realToken.expirationDate < datetime.utcnow()):
+        return False
+
+    if isAdmin:
+        try:
+            Admin.get(Admin.user == realToken.user)
+        except:
+            return False
+
+    return True  # returning means that we have a valid token
+
+
+def requireAuth(view):
+    @wraps(view)
+    def require_auth(*args, **kwargs):
+        token = None
+        try:
+            token = request.headers['Authorization']
+        except:
+            raise GeneralApiException("Access denied", status_code=200)
+
+        if not validateAuthToken(token, True):
+            raise GeneralApiException("Access denied", status_code=200)
+
+        return view(*args, **kwargs)
+    return require_auth
+
+
+def requireAdmin(view):
+    @wraps(view)
+    def require_auth(*args, **kwargs):
+        token = None
+        try:
+            token = request.headers['Authorization']
+        except:
+            raise GeneralApiException("Access denied", status_code=200)
+
+        if not validateAuthToken(token, True):
+            raise GeneralApiException("Access denied", status_code=200)
+
+        return view(*args, **kwargs)
+    return require_auth
