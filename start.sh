70706b7eb1fffea1c8cacf46467dafd3586ae256
jghibiki <jghibiki.games@gmail.com>
2015-06-22 15:19:55 -0500

    Updates the workflow for building the project, now requires docker-compose 0.3

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
-#export PATH=~/.gem/ruby/2.2.0/bin:$PATH
-
-#cd ./client/src
-#compass compile
-
-#cd ./../../
 cp ./search-api.key ./api/src/search-api.key
 
-/usr/local/bin/docker-compose build;
+/usr/local/bin/docker-compose build ;
 /usr/local/bin/docker-compose stop;
-/usr/local/bin/docker-compose up -d;
+/usr/local/bin/docker-compose up -d --x-smart-recreate;
