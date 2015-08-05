0d43af4b5df9a398f392f731ff65630c26031cde
jghibiki <jghibiki.games@gmail.com>
2015-08-04 20:55:44 -0500

    have the scripts restart the containers so that new server configs are loaded on deploy

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
 cp ./search-api.key ./api/src/search-api.key
 
+/usr/local/bin/docker-compose -f dev.yml stop;
 /usr/local/bin/docker-compose -f dev.yml up -d --x-smart-recreate;
--- prod.sh
+++ prod.sh
@@ -1,3 +1,4 @@
 cp ./search-api.key ./api/src/search-api.key
 
+/usr/local/bin/docker-compose -f prod.yml up stop;
 /usr/local/bin/docker-compose -f prod.yml up -d --x-smart-recreate;
